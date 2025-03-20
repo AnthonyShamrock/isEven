df4ac6e68bf04e89ee6f52e04d0e10a5c87b2052
AnthonyShamrock <conorhammchs@gmail.com>
2025-03-20 12:12:16 -0400

    Stupid, but it works...

--- jumplist.py
+++ jumplist.py
@@ -1,17 +1,13 @@
-# "Space Eater"
 def isEven(input):
     # Guard Clause: check if digits
     if not input.isdigit():
         print("Must be digits only")
         return __init()
     
-    
-    lastDigit = input[-1]
-    oddNumbers = ["1","3","5","7","9"]
-    return not lastDigit in oddNumbers
+    isEvenNumber = [True, False, True, False, True, False, True, False, True, False, True, False] # You get the point...
+    return isEvenNumber[int(input)]
 
 def __init():
     print("Number is Even" if isEven(input("Enter Number to Check if Even Or Odd:\n").strip()) == True else "Number is Odd")
     return __init()
-
 __init()
