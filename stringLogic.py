b5cb6ff65b52a02b5cb6a8b8db0759594e11d3d0
AnthonyShamrock <conorhammchs@gmail.com>
2025-03-20 12:11:50 -0400

    Fixed tenary operator logic

--- stringLogic.py
+++ stringLogic.py
@@ -2,15 +2,15 @@ def isEven(input):
     # Guard Clause: check if digits
     if not input.isdigit():
         print("Must be digits only")
-        return
+        return __init()
     
     
     lastDigit = input[-1]
     oddNumbers = ["1","3","5","7","9"]
-    return lastDigit in oddNumbers
+    return not lastDigit in oddNumbers
 
 def __init():
-    print("Number is Odd" if isEven(input("Enter Number to Check if Even Or Odd:\n").strip()) == True else "Number is Even")
+    print("Number is Even" if isEven(input("Enter Number to Check if Even Or Odd:\n").strip()) == True else "Number is Odd")
     return __init()
 
 __init()
